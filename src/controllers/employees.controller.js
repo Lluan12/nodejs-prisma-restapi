0e14f09b404b5f2d1014e7499b146ee707112edc
Lluan12 <103333263+Lluan12@users.noreply.github.com>
2024-10-11 02:25:06 -0700

    add logic database product

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -25,13 +25,11 @@ export const getEmployee = async (req, res) => {
 export const createEmployee = async (req, res) => {
 	try {
 		const newEmployee = await prisma.employee.create({
-			data: req.body
-		})
-		if(!newEmployee)
-			return res.status(404).json({ message: 'Employee not found' })
-		res.json(newEmployee)
-	} catch(error) {
-		res.status(500).json({ message: 'Employee not created', error })
+			data: req.body,
+		});
+		res.json(newEmployee);
+	} catch (error) {
+		res.status(500).json({ message: "Employee not created", error });
 	}
 };
 
@@ -39,26 +37,26 @@ export const updateEmployee = async (req, res) => {
 	try {
 		const updatedEmployee = await prisma.employee.update({
 			where: { id: parseInt(req.params.id) },
-			data: req.body
-		})
+			data: req.body,
+		});
 
-		if(!updatedEmployee)
-			return res.status(404).json({ message: 'Employee not found' })
-		res.json(updatedEmployee)
-	} catch(error) {
-		res.status(500).json({ message: 'Employee not updated', error })
+		if (!updatedEmployee)
+			return res.status(404).json({ message: "Employee not found" });
+		res.json(updatedEmployee);
+	} catch (error) {
+		res.status(500).json({ message: "Employee not updated", error });
 	}
 };
 
 export const deleteEmployee = async (req, res) => {
 	try {
 		const deletedEmployee = await prisma.employee.delete({
-			where: { id: parseInt(req.params.id) }
-		})
-		if(deletedEmployee)
-			return res.status(404).json({ message: 'Employee not found' })
-		res.sendStatus(204)
+			where: { id: parseInt(req.params.id) },
+		});
+		if (deletedEmployee)
+			return res.status(404).json({ message: "Employee not found" });
+		res.sendStatus(204);
 	} catch (error) {
-		res.status(500).json({ message: 'Employee not deleted' })
+		res.status(500).json({ message: "Employee not deleted" });
 	}
 };
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,22 +1,61 @@
-import prisma from '../db/connection.js'
+import prisma from "../db/connection.js";
 
-export const getProducts = async(req, res) => {
+export const getProducts = async (req, res) => {
+	try {
+		const products = await prisma.product.findMany();
+		res.json(products);
+	} catch (error) {
+		res.status(500).json({ message: "Product not found", error });
+	}
+};
 
-}
+export const getProduct = async (req, res) => {
+	try {
+		const product = await prisma.product.findUnique({
+			where: { id: parseInt(req.params.id) },
+		});
+		if (!product)
+			return res.status(404).json({ message: "Product not found" });
+		res.json(product);
+	} catch (error) {
+		res.status(500).json({ message: "Product not found", error });
+	}
+};
 
-export const getProduct = async(req, res) => {
+export const createProduct = async (req, res) => {
+	try {
+		const newProduct = await prisma.product.create({
+			data: req.body,
+		});
+		res.json(newProduct);
+	} catch (error) {
+		res.status(500).json({ message: "Product not created", error });
+	}
+};
 
-}
+export const updateProduct = async (req, res) => {
+	try {
+		updatedProduct = await prisma.product.update({
+			where: { id: parseInt(req.params.id) },
+			data: req.body,
+		});
+		if (!updatedProduct)
+			return res.status(404).json({ message: "Product not found" });
+		res.json(updateProduct);
+	} catch (error) {
+		res.status(500).json({ message: "Product not updated", error });
+	}
+};
 
-export const createProduct = async(req, res) => {
-
-}
-
-export const updateProduct = async(req, res) => {
-
-}
-
-export const deleteProduct = async(req, res) => {
-
-}
-
+export const deleteProduct = async (req, res) => {
+	try {
+		deletedProduct = await prisma.product.delete({
+			where: { id: parseInt(req.params.id) },
+		});
+		if (!deleteProduct)
+			return res.status(404).json({ message: "Product not found" });
+		res.sendStatus(204);
+	} catch (error) {
+		res.status(500).json({ message: "Product not deleted", error });
+	}
+};
